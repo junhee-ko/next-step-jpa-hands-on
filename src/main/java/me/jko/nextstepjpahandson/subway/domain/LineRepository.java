5aa7b37c45d1d717141a898a0bfc96bf224cbb2c
Junhee Ko <jko@rainist.com>
2021-05-27 23:20:04 +0900

    Add LineRepository

---
+++ src/main/java/me/jko/nextstepjpahandson/subway/domain/LineRepository.java
@@ -0,0 +1,7 @@
+package me.jko.nextstepjpahandson.subway.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LineRepository extends JpaRepository<Line, Long> {
+
+}
